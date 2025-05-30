0887599298f9c14b30bae335857b8c2fe9e2d52e
Ravi Vadher <vadherravi67@gmail.com>
2025-05-30 14:31:47 +0530

    Feature: added orderConfirmation api for testing.

---
+++ pages/api/orderConfirmation.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type ResponseData = {
+  success: boolean
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  res.status(200).json({ success: true })
+}
