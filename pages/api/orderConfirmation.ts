8fbd236ead05f864e159630ab5257ca1f748ed2b
Deepak Kumar <dkumar@horizontalintegration.com>
2025-05-31 20:47:33 +0530

    change in sfmc code

--- pages/api/orderConfirmation.ts
+++ pages/api/orderConfirmation.ts
@@ -8,10 +8,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   const body = req.body
   const orderId: string = body?.RouteParams?.orderID
   const octoken: string = body?.AnonUserToken
-  let result: boolean = false
+  const result: boolean = false
 
- await axios.post('https://webhook.site/39c63458-f6c4-43ef-8ed7-fd71e556f341', req.body);
-
+  //await axios.post('https://webhook.site/39c63458-f6c4-43ef-8ed7-fd71e556f341', req.body)
 
   if (orderId && octoken && process.env.NEXT_PUBLIC_ET_CLIENTSECRET) {
     console.log(
@@ -19,87 +18,79 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         process.env?.NEXT_PUBLIC_OC_BASE_API_URL
       }|${process.env?.NEXT_PUBLIC_ET_CLIENTSECRET?.slice(-5)}`
     )
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    let worksheet: any = undefined
-    await axios
-      .get(`${process.env.NEXT_PUBLIC_OC_BASE_API_URL}/v1/orders/All/${orderId}/worksheet`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${octoken}`,
-        },
-      })
-      .then((response) => {
-        if (response?.status == 200 && response.data) {
-          worksheet = response.data
-          if (worksheet) {
-            console.log(`Worksheet found ${orderId}`)
-             axios
-              .post('https://mcyl0bsfb6nnjg5v3n6gbh9v6gc0.auth.marketingcloudapis.com/v2/token', {
-                grant_type: 'client_credentials',
-                client_id: 'lf5r1l4pmw47mabwh3qmk6gq',
-                client_secret: process.env.NEXT_PUBLIC_ET_CLIENTSECRET,
-                account_id: '100016735',
-              })
-              //.then((response) => console.log(response))
-              .then((response) => {
-                console.log(`sfmc token response ${response?.status}`)
-                if (response?.status == 200) {
-                  SendBookingConfirmationEmail(worksheet, response?.data?.access_token)
-                  result = true
-                }
-              })
-              .then((error) => console.log(error))
-          } else {
-            console.log(`Worksheet null ${orderId}`)
-          }
-        } else {
-          console.log(`Worksheet response ${response?.status} | ${response.data}`)
-        }
-      })
-      .then((error) => console.log(error))
+
+    const worksheet = await GetWorksheet(orderId, octoken)
+    if (worksheet) {
+      const sfmctoken = await GetSfmcToken()
+      if (sfmctoken) {
+        const emailsent = await SendBookingConfirmationEmail(worksheet, sfmctoken)
+        console.log(emailsent)
+      } else {
+        console.log('failed to get sfmctoken')
+      }
+    } else {
+      console.log(`Worksheet not found ${orderId}`)
+    }
   } else {
-    console.log('Payload has missing order data')
+    console.log('orderId or octoken or NEXT_PUBLIC_ET_CLIENTSECRET Missing!')
   }
   res.status(200).json({ success: result })
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-function SendBookingConfirmationEmail(worksheet: any, sfmctoken: string) {
-  if (sfmctoken) {
-    console.log(`SendBookingConfirmationEmail`)
-    const vehicle = worksheet.LineItems[0]
-    const name = vehicle.xp.name.split(' ')
-    axios
-      .post(
-        'https://mcyl0bsfb6nnjg5v3n6gbh9v6gc0.rest.marketingcloudapis.com/interaction/v1/events',
-        {
-          ContactKey: 'dkumar@horizontal.com',
-          EventDefinitionKey: 'APIEvent-cf204840-2ced-ab8a-a103-4a4bb3f27119',
-          Data: {
-            email: vehicle.xp.email,
-            firstname: name[0],
-            lastname: name.length > 1 ? name[1] : '',
-            bookingid: worksheet.Order.ID,
-            bookingamount: vehicle.xp.bookingAmount ?? '2000.00',
-            vehicletype: 'Chetak',
-            vehiclevariant: vehicle.xp.model ?? '3501',
-            bookingdate: new Date().toDateString(),
-          },
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${sfmctoken}`,
-          },
-        }
-      )
-      .then((response) => {
-        if (response?.status == 201) {
-          console.log(response.data)
-        }
-      })
-      .then((error) => console.log(error))
-  } else {
-    console.log('Token Missing in token response')
-  }
+async function GetWorksheet(orderId: string, octoken: string) {
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_OC_BASE_API_URL}/v1/orders/All/${orderId}/worksheet`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${octoken}`,
+      },
+    }
+  )
+  return response?.data
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function GetSfmcToken() {
+  const response = await axios.post(
+    'https://mcyl0bsfb6nnjg5v3n6gbh9v6gc0.auth.marketingcloudapis.com/v2/token',
+    {
+      grant_type: 'client_credentials',
+      client_id: 'lf5r1l4pmw47mabwh3qmk6gq',
+      client_secret: process.env.NEXT_PUBLIC_ET_CLIENTSECRET,
+      account_id: '100016735',
+    }
+  )
+  return response?.data?.access_token
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function SendBookingConfirmationEmail(worksheet: any, sfmctoken: string) {
+  const vehicle = worksheet.LineItems[0]
+  const name = vehicle.xp.name.split(' ')
+  const response = await axios.post(
+    'https://mcyl0bsfb6nnjg5v3n6gbh9v6gc0.rest.marketingcloudapis.com/interaction/v1/events',
+    {
+      ContactKey: 'dkumar@horizontal.com',
+      EventDefinitionKey: 'APIEvent-cf204840-2ced-ab8a-a103-4a4bb3f27119',
+      Data: {
+        email: vehicle.xp.email,
+        firstname: name[0],
+        lastname: name.length > 1 ? name[1] : '',
+        bookingid: worksheet.Order.ID,
+        bookingamount: vehicle.xp.bookingAmount ?? '2000.00',
+        vehicletype: 'Chetak',
+        vehiclevariant: vehicle.xp.model ?? '3501',
+        bookingdate: new Date().toDateString(),
+      },
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${sfmctoken}`,
+      },
+    }
+  )
+  return response?.data
 }
